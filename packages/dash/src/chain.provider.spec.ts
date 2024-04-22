e9d386b61ee490dd518daf7dc9b93689e9c1ff8a
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-04-22 21:44:54 +0200

    Create chain.provider.spec.ts

---
+++ packages/dash/src/chain.provider.spec.ts
@@ -0,0 +1,71 @@
+import { DogecoinProvider } from './chain.provider';
+import { IndexerDataSource } from './datasource';
+import { DASG_MANIFEST } from './manifests';
+import { ChainMsg } from './msg';
+
+jest.mock('./datasource/indexer/queries/balances.query', () => ({
+  getBalance: () => {
+    return [];
+  },
+}));
+
+describe('chain.provider', () => {
+  let provider: DashProvider;
+
+  beforeEach(() => {
+    provider = new DashProvider(new IndexerDataSource(DASH_MANIFEST));
+  });
+
+  it('createMsg(): should create message with data', () => {
+    const msg = provider.createMsg({
+      to: 'DPbphsB3Hgb4Q2Sz32e2NoLbmofMNrp1wn',
+      from: 'DPbphsB3Hgb4Q2Sz32e2NoLbmofMNrp1wn',
+      amount: 0.000001,
+    });
+
+    expect(msg).toBeInstanceOf(ChainMsg);
+  });
+
+  it('should throw an error when broadcasting an unsigned tx', async () => {
+    const msg = provider.createMsg({
+      to: 'DPbphsB3Hgb4Q2Sz32e2NoLbmofMNrp1wn',
+      from: 'DPbphsB3Hgb4Q2Sz32e2NoLbmofMNrp1wn',
+      amount: 0.000001,
+    });
+
+    expect(provider.broadcast([msg])).rejects.toThrow();
+  });
+
+  it('should get transactions for an address from the blockchain', async () => {
+    const txData = await provider.getTransactions(
+      'DPbphsB3Hgb4Q2Sz32e2NoLbmofMNrp1wn',
+      0
+    );
+    expect((await txData.getData()).length).toBeGreaterThan(0);
+  });
+
+  it('should get fee options', async () => {
+    const feeOptions = await provider.gasFeeOptions();
+
+    expect(feeOptions?.low).toBeTruthy();
+    expect(feeOptions?.medium).toBeTruthy();
+    expect(feeOptions?.high).toBeTruthy();
+  });
+
+  it('should get a balance', async () => {
+    const balance = await provider.getBalance(
+      'DPbphsB3Hgb4Q2Sz32e2NoLbmofMNrp1wn'
+    );
+
+    const balanceData = await balance.getData();
+    expect(balanceData.length).toEqual(0);
+  });
+
+  it('should throw for a non-existant transaction on the blockchain', async () => {
+    expect(
+      provider.getTransaction(
+        'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+      )
+    ).rejects.toThrow();
+  });
+});
